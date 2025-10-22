530bc341a32b23fd27da0277fd1b098e2810dd5a
Seanix <seanixchannel@gmail.com>
2025-10-23 00:09:54 +1300

    fixed compass position

--- app/map.tsx
+++ app/map.tsx
@@ -165,7 +165,7 @@ const MapComponent = () => {
   return (
     <div
       ref={mapContainerRef}
-      style={{ width: "100%", height: "100vh", top: "0px", left: "0px" }}
+      style={{ width: "100%", height: "100dvh", top: "0px", left: "0px" }}
     />
   );
 };
